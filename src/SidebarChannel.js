4a0d44bc38b7226ef161a404960ef15910cda21c
azatseyrek <azatseyrek@hotmail.com>
2021-10-30 01:02:49 +0300

    chat component start point

---
+++ src/SidebarChannel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SidebarChannel.css'
+const SidebarChannel = ({id, channel}) => {
+    return (
+        <div className='sidebarChannel'>
+            <h4><span className='sidebarChannel__hash'>#</span>Youtube</h4>
+        </div>
+    )
+}
+
+export default SidebarChannel
