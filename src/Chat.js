e20e6c81fd5b7c25c04c7bbec49d54314d7693ba
Azat Seyrek <azatseyrek@hotmail.com>
2021-11-03 22:26:36 +0300

    final

--- src/Chat.js
+++ src/Chat.js
@@ -1,39 +1,85 @@
-import { AddCircle, CardGiftcard, EmojiEmotions, Gif } from "@mui/icons-material";
-import React from "react";
+import {
+  AddCircle,
+  CardGiftcard,
+  EmojiEmotions,
+  Gif,
+} from "@mui/icons-material";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import "./Chat.css";
 import ChatHeader from "./ChatHeader";
 import { selectChannelId, selectChannelName } from "./features/appSlice";
 import { selectUser } from "./features/userSlice";
+import db from "./firebase";
 import Message from "./Message";
+import firebase from "firebase";
 
 const Chat = () => {
+  const user = useSelector(selectUser);
+  const channelId = useSelector(selectChannelId);
+  const channelName = useSelector(selectChannelName);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
 
-const user = useSelector(selectUser)
-const channelId = useSelector(selectChannelId)
-const channelName = useSelector(selectChannelName)
+  useEffect(() => {
+    if (channelId) {
+      db.collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) => 
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, [channelId]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    db.collection("channels").doc(channelId).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      user: user,
+    });
+    setInput("");
+  };
 
   return (
     <div className="chat">
-<ChatHeader channelName = {channelName}/>
-<div className="chat__messages">
-<Message/>
-<Message/>
-<Message/>
-</div>
+      <ChatHeader channelName={channelName} />
+      <div className="chat__messages">
+        {messages.map((message) => (
+          <Message
+          timestamp={message.timestamp}
+          message ={message.message}
+          user={message.user} />
+        ))}
+      </div>
 
-<div className="chat__input">
-<AddCircle fontSize='large'/>
-<form >
-    <input placeholder={`Message #TESTCHANNEL`}/>
-    <button className='chat__inputButton' type='submit'>Send Message</button>
-</form>
-<div className="chat__inputIcons">
-    <CardGiftcard/>
-    <Gif/>
-    <EmojiEmotions/>
-</div>
-</div>
+      <div className="chat__input">
+        <AddCircle fontSize="large" />
+        <form>
+          <input
+            value={input}
+            disabled={!channelId}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder={`Message #${channelName}`}
+          />
+          <button
+            onClick={sendMessage}
+            disabled={!channelId}
+            className="chat__inputButton"
+            type="submit"
+          >
+            Send Message
+          </button>
+        </form>
+        <div className="chat__inputIcons">
+          <CardGiftcard />
+          <Gif />
+          <EmojiEmotions />
+        </div>
+      </div>
     </div>
   );
 };
--- src/Message.js
+++ src/Message.js
@@ -1,19 +1,20 @@
-import { Avatar } from '@mui/material'
-import React from 'react'
-import './Message.css'
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Message.css";
 
-const Message = () => {
-    return (
-        <div className='message'>
-            <Avatar/>
-            <div className="message__info">
-                <h4>Azat
-                <span className='message__timestamp'>this is a timestamp</span>
-                </h4>
-                <p>This is a test message</p>
-            </div>
-        </div>
-    )
-}
+const Message = ({ message, user, timestamp }) => {
+  return (
+    <div className="message">
+      <Avatar src={user.photo} />
+      <div className="message__info">
+        <h4>
+          {user.displayName}
+          <span className="message__timestamp">{new Date(timestamp?.toDate()).toUTCString()}</span>
+        </h4>
+        <p>{message}</p>
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
